db0dbef5f65a5cf6082942bff0a59f0e92d4d9e3
namrapithwa-tech <164359724+namrapithwa-tech@users.noreply.github.com>
2025-02-18 10:29:51 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TagHelpers_Demo_NRV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TagHelpers/MyCustomTagHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers_Demo_NRV.TagHelpers
+{
+    [HtmlTargetElement("Namra")] //Target Element 
+    public class MyCustomTagHelpers:TagHelper
+    {
+        public string path { get; set; }
+        public string altText { get; set; }
+        public string Height { get; set; }
+        public string Width { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img"; // no return in index
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("src", path);
+            output.Attributes.SetAttribute("alt", altText);
+
+            if (!string.IsNullOrEmpty(Width))
+                output.Attributes.SetAttribute("width", Width);
+
+            if (!string.IsNullOrEmpty(Height))
+                output.Attributes.SetAttribute("height", Height);
+            
+            base.Process(context, output);
+        }
+    }
+}
